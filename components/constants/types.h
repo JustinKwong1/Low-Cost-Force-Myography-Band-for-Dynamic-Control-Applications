01ae219d37e2c39b1149c2449e0f4ac0c9219319
Lana <30848390+lwong370@users.noreply.github.com>
2025-05-07 01:11:25 -0700

    Delete components directory

--- components/constants/types.h
+++
@@ -1,9 +0,0 @@
-// components/constants/types.h
-#pragma once
-
-#include <vector>
-#include <array>
-#include "config.h"
-
-using Window = std::vector<std::vector<int>>;
-using MAVFeature = std::array<float, NUM_FEATURES>;
